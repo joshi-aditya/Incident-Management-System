a71016984895e9b0f8d34138fadec5212435d097
Neha Varshney <varshney.n@husky.neu.edu>
2017-11-18 19:18:06 -0500

    With employee and useraccount package

--- AED_Project/src/Business/Employee/EmployeeDirectory.java
+++ AED_Project/src/Business/Employee/EmployeeDirectory.java
@@ -5,10 +5,29 @@
  */
 package Business.Employee;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author joshiaditya
  */
 public class EmployeeDirectory {
+    
+    private ArrayList<Employee> employeeList;
+
+    public EmployeeDirectory() {
+        employeeList = new ArrayList<>();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+    
+     public Employee createEmployee(String name){
+        Employee employee = new Employee();
+        employee.setName(name);
+        employeeList.add(employee);
+        return employee;
+    }
     
 }
--- AED_Project/src/Business/UserAccount/UserAccount.java
+++ AED_Project/src/Business/UserAccount/UserAccount.java
@@ -11,4 +11,25 @@ package Business.UserAccount;
  */
 public class UserAccount {
     
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
 }
