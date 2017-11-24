76504948ae23073cd8e7515a11db61449bdfcba0
joshiaditya <joshi.adit@husky.neu.edu>
2017-11-23 19:21:12 -0500

    work request class

---
+++ AED_Project/src/Business/WorkQueue/IncidentWorkRequest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+
+/**
+ *
+ * @author joshiaditya
+ */
+public class IncidentWorkRequest extends WorkRequest{
+    
+    String address;
+    int zipCode;
+    int incidentId;
+    private static int counter =0;
+
+    public IncidentWorkRequest() {
+        counter ++;
+        incidentId = counter;
+    }
+    
+    public enum IncidentType{
+        
+        Gun_Violence("Gun Violence"),
+        Substance_Abuse("Substance Abuse"),
+        Accident("Accident"),
+        Fire_Incident("Fire Incident");
+        
+        private String value;
+        
+        private IncidentType(String value){
+            this.value = value;
+        }
+        
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+        
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public int getIncidentId() {
+        return incidentId;
+    }
+
+    public void setIncidentId(int incidentId) {
+        this.incidentId = incidentId;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void setCounter(int counter) {
+        IncidentWorkRequest.counter = counter;
+    }
+    
+    @Override
+    public String toString() {
+        return address;
+    }
+}
